48e989c5d5041f52654a84b0973958136ca9bedb
Hugo Uchôas Borges <hugoborges@unifei.edu.br>
2017-10-29 21:09:25 -0200

    [ADD] Added down, left and right movement to capsule.

--- Space-Spelling-Shooter/Assets/CapsuleCharacter.cs
+++ Space-Spelling-Shooter/Assets/CapsuleCharacter.cs
@@ -11,9 +11,21 @@ public class CapsuleCharacter : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKey(KeyCode.UpArrow))
+        if (Input.GetKey(KeyCode.UpArrow))
         {
-            gameObject.GetComponent<Rigidbody>().transform.Translate(0,0,0.25f);
+            gameObject.GetComponent<Rigidbody>().transform.Translate(0, 0, 0.25f);
         }
-	}
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            gameObject.GetComponent<Rigidbody>().transform.Translate(0, 0, 0.25f);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            gameObject.GetComponent<Rigidbody>().transform.Translate(-0.25f, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            gameObject.GetComponent<Rigidbody>().transform.Translate(0.25f, 0, 0);
+        }
+    }
 }
