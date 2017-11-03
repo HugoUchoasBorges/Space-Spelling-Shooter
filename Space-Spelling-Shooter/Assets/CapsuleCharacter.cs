ee10fdfbff8d30551df2e2d5afed00f106f4adaf
Hugo Uchôas Borges <hugoborges@unifei.edu.br>
2017-11-03 10:59:39 -0200

    [FIX] Correção feita na movimentação do personagem

--- Space-Spelling-Shooter/Assets/CapsuleCharacter.cs
+++ Space-Spelling-Shooter/Assets/CapsuleCharacter.cs
@@ -13,19 +13,19 @@ public class CapsuleCharacter : MonoBehaviour {
 	void Update () {
         if (Input.GetKey(KeyCode.UpArrow))
         {
-            gameObject.GetComponent<Rigidbody>().transform.Translate(0, 0, 0.25f);
+            gameObject.GetComponent<Rigidbody>().transform.Translate(0, 0, 0.20f);
         }
         if (Input.GetKey(KeyCode.DownArrow))
         {
-            gameObject.GetComponent<Rigidbody>().transform.Translate(0, 0, -0.25f);
+            gameObject.GetComponent<Rigidbody>().transform.Translate(0, 0, -10f);
         }
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            gameObject.GetComponent<Rigidbody>().transform.Translate(-0.25f, 0, 0);
+            gameObject.GetComponent<Rigidbody>().transform.Translate(-0.20f, 0, 0);
         }
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            gameObject.GetComponent<Rigidbody>().transform.Translate(0.25f, 0, 0);
+            gameObject.GetComponent<Rigidbody>().transform.Translate(0.20f, 0, 0);
         }
     }
 }
