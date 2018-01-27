efbb329106eccb0dd11f618799dcd5f66720315d
Hugo Uchôas Borges <hugoborges@unifei.edu.br>
2018-01-27 21:48:45 -0200

    [ADD] Player se comportando como se o cenário fosse infinito

--- Space-Spelling-Shooter/Assets/Scripts/player/Movimentacao.cs
+++ Space-Spelling-Shooter/Assets/Scripts/player/Movimentacao.cs
@@ -6,6 +6,13 @@ public class Movimentacao : MonoBehaviour {
 
     //Declaração das Variáveis
 
+    // Variáveis com a nova posição do player ao chegar no limite do cenário
+    float newX;
+    float newY;
+
+    //É o tamanho da zona fora da tela onde objetos podem se movimentar
+    float deadZone;
+
     public Rigidbody2D rigidBody2D;
 
     //Variáveis para controle do personagem
@@ -27,7 +34,9 @@ public class Movimentacao : MonoBehaviour {
         rigidBody2D.drag = 0.3f;
         impulseThreshold = 50;
         rotationThreshold = 8;
-}
+
+        deadZone = GetComponent<CircleCollider2D>().radius;
+    }
 
     // Update is called once per frame
     void Update()
@@ -35,6 +44,30 @@ public class Movimentacao : MonoBehaviour {
         //Captura de entrada para movimentação do player
         inputImpulse = Input.GetAxis("Vertical");
         inputRotation = -Input.GetAxis("Horizontal");
+
+        //Tratamento das bordas do cenário
+        verificaBordas();
+
+    }
+
+    void verificaBordas()
+    {
+        Vector2 newPosition = transform.position;
+
+        // Lidando com os limites do cenário
+        if (Mathf.Abs(transform.position.x) > GeradorDeArestas.bottomRightCorner.x + deadZone)
+            if(transform.position.x > 0)
+                newPosition.x = GeradorDeArestas.bottomLeftCorner.x;
+            else
+                newPosition.x = GeradorDeArestas.bottomRightCorner.x;
+
+        if (Mathf.Abs(transform.position.y) > GeradorDeArestas.upperRightCorner.y + deadZone)
+            if (transform.position.y > 0)
+                newPosition.y = GeradorDeArestas.bottomRightCorner.y;
+            else
+                newPosition.y = GeradorDeArestas.upperRightCorner.y;
+
+        transform.position = newPosition;
 
     }
 
