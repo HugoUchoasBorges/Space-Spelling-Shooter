b8c65a57a4a599ce6dbd312519a78a250acf6760
Hugo Uchôas Borges <hugoborges@unifei.edu.br>
2018-01-30 00:19:21 -0200

    [ADD] Classe Singleton para lidar com variáveis globais

---
+++ Space-Spelling-Shooter/Assets/Scripts/GlobalVariables.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalVariables : Singleton<GlobalVariables> {
+
+    protected GlobalVariables() { } // guarantee this will be always a singleton only - can't use the constructor!
+
+    public static int totalVidas = 3;
+}
