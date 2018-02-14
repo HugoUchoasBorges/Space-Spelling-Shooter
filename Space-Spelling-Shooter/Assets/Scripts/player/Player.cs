0ed177579ab6fed3586db1ffba3c5f9ec38b32ab
Hugo Uchôas Borges <hugoborges@unifei.edu.br>
2018-02-14 16:50:48 -0200

    [ADD][IMP] Classe **Player** para cuidar das variáveis e métodos importantes dos inimigos

---
+++ Space-Spelling-Shooter/Assets/Scripts/player/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public GerenciadorJogo gerenciadorJogo;
+    public SistemaDigitacao sistemaDigitacao;
+    public MovimentacaoPlayer movimentacao;
+
+	// Use this for initialization
+	void Start () {
+
+        movimentacao = gameObject.AddComponent<MovimentacaoPlayer>();
+        gerenciadorJogo = gameObject.AddComponent<GerenciadorJogo>();
+        sistemaDigitacao = gameObject.AddComponent<SistemaDigitacao>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
