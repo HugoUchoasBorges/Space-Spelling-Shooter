1787915341e479815742c8db02316302b35369b6
Hugo Uchôas Borges <hugoborges@unifei.edu.br>
2018-02-24 14:21:42 -0300

    [IMP] Projéteis seguem inimigo

--- Space-Spelling-Shooter/Assets/Scripts/player/BulletController.cs
+++ Space-Spelling-Shooter/Assets/Scripts/player/BulletController.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class BulletController : MonoBehaviour {
 
     public static float bulletSpeed = 8f;
+    public static GameObject target;
 
     // Use this for initialization
     void Start () {
-        bulletSpeed = 8f;
+        bulletSpeed = 20f;
+        target = TypingSystem.target;
     }
 	
 	// Update is called once per frame
@@ -17,8 +19,11 @@ public class BulletController : MonoBehaviour {
         // Get the bullet's current position
         Vector2 position = transform.position;
 
+        float step = bulletSpeed * Time.deltaTime;
+
         // compute the bullet's new position
         position = new Vector2(position.x, position.y + bulletSpeed * Time.deltaTime);
+        position = Vector2.MoveTowards(transform.position, target.transform.position, step);
 
         // update the bullet's position
         transform.position = position;
@@ -27,7 +32,7 @@ public class BulletController : MonoBehaviour {
         Vector2 max = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
 
         // if the bullet went outside the screen on the top, then destroy it
-        if (transform.position.y > max.y)
+        if (transform.position == target.transform.position)
         {
             Destroy(gameObject);
         }
