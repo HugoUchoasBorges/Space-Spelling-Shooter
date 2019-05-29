4f7b561726fb6df1ea12cacdbe7f80646b8ae6c2
Hugo Uchôas Borges <hugoborges@unifei.edu.br>
2019-05-28 23:21:18 -0300

    [ADD] Screen Boundaries

---
+++ SSShooter/Assets/Scripts/Screen/ScreenBoundaries.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenBoundaries : MonoBehaviour
+{
+
+    #region Variables
+
+    private Vector3 screenBounds;
+
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+    }
+
+    private void limitMovement()
+    {
+        Vector3 objectPos = transform.position;
+        objectPos.x = Mathf.Clamp(objectPos.x, -screenBounds.x, screenBounds.x);
+        objectPos.y = Mathf.Clamp(objectPos.y, -screenBounds.y, screenBounds.y);
+
+        transform.position = objectPos;
+    }
+
+    void LateUpdate()
+    {
+        limitMovement();
+    }
+}
