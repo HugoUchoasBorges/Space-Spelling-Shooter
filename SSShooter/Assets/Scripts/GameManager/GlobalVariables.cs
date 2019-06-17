f3967bb2eb8eb36aa4e29b126ce3d4913f865866
Hugo Uchôas Borges <hugoborges@unifei.edu.br>
2019-06-17 08:13:49 -0300

    [IMP] Better Singleton

--- SSShooter/Assets/Scripts/GameManager/GlobalVariables.cs
+++ SSShooter/Assets/Scripts/GameManager/GlobalVariables.cs
@@ -1,40 +1,7 @@
 ﻿using UnityEngine;
 
-public class GlobalVariables : MonoBehaviour
+public class GlobalVariables : Singleton<GlobalVariables>
 {
-    #region Singleton
-
-    private static GlobalVariables _instance;
-    
-    private static object _lock = new object();
-
-    public static GlobalVariables Instance
-    {
-        get
-        {
-            lock (_lock)
-            {
-                if (_instance == null)
-                {
-                    _instance = FindObjectOfType(typeof(GlobalVariables)) as GlobalVariables;
-                }
-
-                if (_instance == null)
-                {
-                    GameObject singleton = new GameObject();
-                    _instance = singleton.AddComponent<GlobalVariables>();
-
-                    singleton.name = "(singleton) GlobalVariables";
-                    
-                    DontDestroyOnLoad(singleton);
-                }
-
-                return _instance;
-            }
-        }
-    }
-
-    #endregion
 
     #region Variables
     
---
+++ SSShooter/Assets/Scripts/Singleton.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T: MonoBehaviour
+{
+    private static T _instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if (!_instance)
+            {
+                _instance = FindObjectOfType(typeof(T)) as T;
+                if (!_instance)
+                {
+                    GameObject singleton = new GameObject();
+                    _instance = singleton.AddComponent<T>();
+                    singleton.name = "(Singleton)" + typeof(T);
+                    
+                    DontDestroyOnLoad(singleton);
+                }
+            }
+
+            return _instance;
+        }
+    }
+
+    private static bool isQuittingGame;
+
+    private void OnDestroy()
+    {
+        isQuittingGame = true;
+    }
+}
