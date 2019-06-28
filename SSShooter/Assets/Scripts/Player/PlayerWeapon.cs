ec8fd519a63fcc78f5b257b6059286898a283bbf
Hugo Uchôas Borges <hugoborges@unifei.edu.br>
2019-06-28 08:18:37 -0300

    [ADD] Particle System

--- SSShooter/Assets/Scripts/Player/PlayerWeapon.cs
+++ SSShooter/Assets/Scripts/Player/PlayerWeapon.cs
@@ -1,4 +1,5 @@
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.Assertions;
 
 public class PlayerWeapon : MonoBehaviour
 {
@@ -8,14 +9,21 @@ public class PlayerWeapon : MonoBehaviour
     public GameObject bullet;
     [Range(1f,20f)]
     public float bulletSpeed;
+    public Transform weaponPosition;
 
     #endregion
-    
+
+    private void Awake()
+    {
+        Assert.IsNotNull(weaponPosition, "WeaponPosition Transform not found");
+    }
+
     public void Shoot(Transform target)
     {
         EnemyDisplay enemy = target.GetComponent<EnemyDisplay>();
-        
-        GameObject newBullet = Instantiate(bullet, transform.position, Quaternion.identity);
+
+        Vector3 position = weaponPosition ? weaponPosition.position : transform.position;
+        GameObject newBullet = Instantiate(bullet, position, transform.rotation);
         BulletController bulletController = newBullet.GetComponent<BulletController>();
         if (!bulletController)
         {
